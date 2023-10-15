2eccad081cdfe1d36fe329eb62ec1e8f619cc762
Goncharov <GVVic@mail.ru>
2023-10-15 10:08:38 +0700

    Prepare_project

--- FunalWorkForJava/src/main/java/ru/gb/goncharov/Main.java
+++ FunalWorkForJava/src/main/java/ru/gb/goncharov/Main.java
@@ -3,6 +3,6 @@ package ru.gb.goncharov;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
-
+        System.out.println("TestingGit!");
     }
 }
